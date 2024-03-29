3bc45388f1821cff5c9937c4668902f374d818c9
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-03-29 21:13:38 +0530

    making Header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import Drower from "../Drower/Drower";
+import "./Header.css"
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+  const [cheak, setcheak] = useState(false);
+  const OnToggle = () => {
+    setToggle(!toggle);
+    setcheak(!cheak);
+  };
+  return (
+    <div className="fullNav">
+     <Drower toggle={toggle} OnToggle={OnToggle} />
+      <Navbar OnToggle={OnToggle} cheak={cheak} setcheak={setcheak} />
+    </div>
+  );
+};
+
+export default Header;
